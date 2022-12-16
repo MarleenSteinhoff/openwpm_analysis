127f834daef9b8cbdc3df0d5c24783ee502aabf5
MarleenSteinhoff <marleensteinhoff@gmail.com>
2022-12-16 19:45:13 +0100

    bash script works!

--- download-batch-process.sh
+++ download-batch-process.sh
@@ -2,8 +2,8 @@
 #set -e
 
 # Preprocess and analyze compressed crawl databases
-EXTRACTION_DIR="/tmp/census_tmp"
-
+EXTRACTION_DIR="/tmp/census_data_lz4/extractiondir"
+CODE_DIR="/home/fsadmin/analysis/openwpm_analysis"
 CENSUS_LZ4_DATA_PATH="/tmp/census_data_lz4"
 ROOT_OUT_DIR="/crawler/results"
 
@@ -37,35 +37,37 @@ urls=' "https://webtransparency.cs.princeton.edu/webcensus/data-release/data/sta
 
 
 function decompress_and_process(){
-  echo "Downloading file to process..."
-  ARCHIVE_BASE_NAME=$(basename "$1")
-  CRAWL_NAME=${ARCHIVE_BASE_NAME.tar.lz4}
-  CRAWL_DATA_PATH=$EXTRACTION_DIR/$CRAWL_NAME
+echo "Processing downloaded file..."  
+ARCHIVE_BASE_NAME=$(basename "$1")
+  FILE_NAME="${ARCHIVE_BASE_NAME%.*}"
+  
   echo "Will extract $1 to $CRAWL_DATA_PATH"
-  time lz4 -qdc --no-sparse $1 | tar xf - -C $EXTRACTION_DIR
-  python process_crawl_data.py $CRAWL_DATA_PATH $ROOT_OUT_DIR
+  echo "$FILE_NAME"
+  cd $EXTRACTION_DIR
+  time lz4 -qdc --no-sparse $1 > $FILE_NAME.sqlite
+  rm -f *.lz4
+  #time lz4 -qdc --no-sparse $1 | tar xf - -C $EXTRACTION_DIR
+  echo "File successfully extracted"
+  #only if crawl is new
+  #python process_crawl_data.py $EXTRACTION_DIR/$CRAWL_NAME $ROOT_OUT_DIR
   echo "Size before vacuuming"
-  ls -hl $EXTRACTION_DIR/*201*/201*.sqlite
-  time sqlite3 $EXTRACTION_DIR/*201*/*201*.sqlite 'PRAGMA journal_mode = OFF; PRAGMA synchronous = OFF; PRAGMA page_size = 32768; VACUUM;'
+  ls -hl $EXTRACTION_DIR/*.sqlite
+  time sqlite3 $EXTRACTION_DIR/*.sqlite 'PRAGMA journal_mode = OFF; PRAGMA synchronous = OFF; PRAGMA page_size = 32768; VACUUM;'
   echo "Size after vacuuming"
-  ls -hl $EXTRACTION_DIR/*201*/*201*.sqlite
-  python analyze_crawl.py $CRAWL_DATA_PATH $ROOT_OUT_DIR
-
-  mkdir -p $CENSUS_NORMALIZED_LZ4_DATA_PATH/$2
+  ls -hl $EXTRACTION_DIR/*.sqlite
+  cd $CODE_DIR
+  python analyze_crawl.py $EXTRACTION_DIR $ROOT_OUT_DIR
+  mkdir -p $CENSUS_NORMALIZED_LZ4_DATA_PATH/$CRAWL_NAME
   OUT_NORMALIZED_ARCHIVE=$EXTRACTION_DIR/$ARCHIVE_BASE_NAME
+  exit
   pushd .
   cd $EXTRACTION_DIR
-  tar c *201* | lz4 -9zq - $OUT_NORMALIZED_ARCHIVE
-  popd
-  scp $OUT_NORMALIZED_ARCHIVE odin://mnt/10tb2/census-release-normalized/$2/
-  rm $OUT_NORMALIZED_ARCHIVE
+  echo "SUCCESS!"
   echo "Will remove $EXTRACTION_DIR/*201*"
   rm -rf $EXTRACTION_DIR/*201*
 }
 
 
-for crawl_archive_lz4 in $CENSUS_LZ4_DATA_PATH/$1/*.tar.lz4
+for crawl_archive_lz4 in $CENSUS_LZ4_DATA_PATH/*.lz4
   do decompress_and_process $crawl_archive_lz4 $1
 done;
-
-
