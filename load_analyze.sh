c6388b4754c477660b671222f6c82c6869789ecd
MarleenSteinhoff <marleensteinhoff@gmail.com>
2022-12-17 03:58:31 +0100

    complete download and analysis works

--- load_analyze.sh
+++ load_analyze.sh
@@ -7,7 +7,7 @@ CODE_DIR="/home/fsadmin/analysis/openwpm_analysis"
 CENSUS_LZ4_DATA_PATH="/crawler/census_data_lz4"
 ROOT_OUT_DIR="/crawler/results"
 DOWNLOAD_URL_PATH="/crawler/census_data_lz4/urls.txt"
-
+LZ4_FILE_NAME=""
 CENSUS_NORMALIZED_LZ4_DATA_PATH=${ROOT_OUT_DIR}/normalized/
 mkdir -p $CENSUS_NORMALIZED_LZ4_DATA_PATH
 
@@ -15,21 +15,20 @@ mkdir -p $CENSUS_NORMALIZED_LZ4_DATA_PATH
 function download(){
   echo "Downloading into $CENSUS_LZ4_DATA_PATH"
   cd $CODE_DIR
-  python download_file.py $1 $EXTRACTION_DIR
+  python3 download_file.py $1 $EXTRACTION_DIR
 }
 
 function decompress_and_process(){
-echo "Processing downloaded file..." 
-cd $EXTRACTION_DIR
-ARCHIVE_BASE_NAME=$(basename "$1")
-FILE_NAME="${ARCHIVE_BASE_NAME%.*}"
-FULL_FILE_NAME=${EXTRACTION_DIR/.lz4}
+  echo "Processing downloaded file..."
+  cd $EXTRACTION_DIR 
+  LZ4_FILE_NAME=$(find . -name "*lz4")
+  FILE_NAME=${LZ4_FILE_NAME%.*} 
+
+  echo "Will extract $LZ4_FILE_NAME"
   
-  
-  echo "Will extract $FULL_FILE_NAME to $CRAWL_DATA_PATH"
-  echo "$FILE_NAME"
-  time lz4 -qdc --no-sparse $FULL_FILE_NAME > $FILE_NAME.sqlite
+  time lz4 -qdc --no-sparse $EXTRACTION_DIR/$LZ4_FILE_NAME > "2018.sqlite"
   rm -f *.lz4
+  echo "-----------------------------------------------------"
   #time lz4 -qdc --no-sparse $1 | tar xf - -C $EXTRACTION_DIR
   echo "File successfully extracted"
   #only if crawl is new
@@ -40,6 +39,7 @@ FULL_FILE_NAME=${EXTRACTION_DIR/.lz4}
   echo "Size after vacuuming"
   ls -hl $EXTRACTION_DIR/*.sqlite
   cd $CODE_DIR
+  echo "------------------------------------------------------"
   python analyze_crawl.py $EXTRACTION_DIR $ROOT_OUT_DIR
   mkdir -p $CENSUS_NORMALIZED_LZ4_DATA_PATH/$CRAWL_NAME
   OUT_NORMALIZED_ARCHIVE=$EXTRACTION_DIR/$ARCHIVE_BASE_NAME
@@ -53,7 +53,7 @@ FULL_FILE_NAME=${EXTRACTION_DIR/.lz4}
 
 while IFS= read -r line
 do
-	echo "$line"
+  echo "$line"
   download $line
   decompress_and_process $crawl_archive_lz4
   echo "$line"
