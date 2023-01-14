c8b2d0bc9b8fe35afce3d499c3b86a119403505b
Hm-Steinhof <marleen.steinhoff@hm.edu>
2023-01-14 21:27:10 +0100

    syntax fix

--- extract_features.py
+++ extract_features.py
@@ -360,14 +360,18 @@ def get_cookies(db_file, id_urls_map=tuple(), max_rank=None):
                          ON sv.visit_id = js.visit_id WHERE js.visit_id IN {format(id_urls_map)} AND js.is_session = 0 AND js.is_domain = 0;
                          """
 
-    if CRAWL_NAME not in ["2016-03", "2016-04", "2016-05", "2016-06", "2016-08", "2016-09", "2017-01", "2017-02",
+    if CRAWL_NAME in ["2016-03", "2016-04", "2016-05", "2016-06", "2016-08", "2016-09", "2017-01", "2017-02",
                           "2017-03"]:
-        query_session = f"""SELECT js.visit_id, js.isHttpOnly, sv.site_url, js.baseDomain,
-                         FROM profile_cookies as js LEFT JOIN site_visits as sv
+        query_session = f"""SELECT js.visit_id FROM profile_cookies as js LEFT JOIN site_visits as sv
                          ON sv.visit_id = js.visit_id WHERE js.visit_id IN {format(id_urls_map)} AND js.is_session = 1;
                          """
-    session_df = pd.read_sql_query(query_session, db)
-    num_session_cookies = session_df["visit_id"].size
+    else:
+        query_session = f"""SELECT js.visit_id, js.is_session FROM javascript_cookies WHERE js.visit_id IN {format(id_urls_map)} AND js.is_session = 1;
+                                 """
+
+        session_df = pd.read_sql_query(query_session, db)
+        num_session_cookies = session_df["visit_id"].size
+
     print("session_cookies calculated")
 
     print("Starting get_cookie analysis")
